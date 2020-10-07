e28deac225c296f7600b1969f967deb102328531
Akhilzero <akhilzeroat7@gmail.com>
2020-10-07 11:49:48 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+* Main templete file
+
+* @package Aquila
+
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+</head>
+
+<body>
+    <h1>Hello world</h1>
+</body>
+
+</html>
